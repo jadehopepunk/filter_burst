11077114becdb0848fa9c40aeaafd45fcf185390
craigambrose <craig@craigambrose.com>
2016-11-14 13:13:20 +1300

    moar certbot

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule FilterBurst.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def certbot(conn, _params) do
+    conn
+    |> send_resp(201, "3Z7gsxr7XQ6cGmLacIDJaj40OI-nM0SL5cl8U1Wg-f4.EB8a_7AKIgSI6AnjX1h-foe7zbWNIPSzqYYM4q14uUo")
+  end
 end
