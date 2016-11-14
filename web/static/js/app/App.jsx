a1e31a8d8d5f7b849d19537f7ee26c405f0fc978
craigambrose <craig@craigambrose.com>
2016-11-14 14:28:40 +1300

    added a blurb

--- web/static/js/app/App.jsx
+++ web/static/js/app/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 import NavBar from './NavBar'
 import StoryList from './StoryList'
 import FeedSettings from './FeedSettings'
@@ -7,6 +8,20 @@ const App = (props) => (
   <div>
     <NavBar />
     <div className="container">
+      <div className={"alert alert-info " + css(styles.notice)}>
+        <h4>How do we fix social media?</h4>
+        <p>
+          It feels like the US has been torn in half. How do we social media conversations
+          with people different to us, when it&#39;s already hard enough to talk online
+          without abuse? Is facebook going to make that happen for us?
+        </p>
+        <p>
+          This is an experiment to try doing it ourselves. If you have any ideas on how
+          to make online conversation more <b>diverse</b> or more <b>safe</b> then
+          list them on our <a href="https://filterburst.useresponse.com/">idea voting page</a>.
+        </p>
+      </div>
+
       <div className="row">
         <div className="col-md-8">
           <StoryList />
@@ -18,5 +33,11 @@ const App = (props) => (
     </div>
   </div>
 );
+
+const styles = StyleSheet.create({
+  notice: {
+    // padding: 10,
+  }
+})
 
 export default App;
--- web/static/js/app/NavBar.jsx
+++ web/static/js/app/NavBar.jsx
@@ -10,21 +10,9 @@ const NavBar = (props) => (
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      {/*<Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>*/}
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="https://filterburst.useresponse.com/">Participate</a></li>
+      </ul>
     </Navbar.Collapse>
   </Navbar>
 );
