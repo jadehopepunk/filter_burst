6a3bb40e72286d6d7a5c55bf8ab5ee2f723f69d1
craigambrose <craig@craigambrose.com>
2016-11-17 21:57:15 +1300

    added facebook login button

--- web/static/js/app/NavBar.jsx
+++ web/static/js/app/NavBar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import FacebookLogin from 'react-facebook-login'
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 const NavBar = (props) => (
   <Navbar inverse collapseOnSelect>
@@ -12,6 +17,15 @@ const NavBar = (props) => (
     <Navbar.Collapse>
       <ul className="nav navbar-nav navbar-right">
         <li><a href="https://filterburst.useresponse.com/">Participate</a></li>
+        <li>
+          <FacebookLogin
+            appId="1288160917871988"
+            autoLoad={true}
+            fields="id,name,email"
+            scope="public_profile,email,user_friends"
+            cssClass="btn btn-link btn-facebook"
+            callback={responseFacebook} />
+        </li>
       </ul>
     </Navbar.Collapse>
   </Navbar>
