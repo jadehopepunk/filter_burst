f61667a80057b9641e68afcf66f12291b2fdaf7a
craigambrose <craig@craigambrose.com>
2016-11-24 22:03:34 +1300

    we know if we're logged in

--- web/static/js/auth/LoginStatus.jsx
+++ web/static/js/auth/LoginStatus.jsx
@@ -6,10 +6,13 @@ import * as Actions from './actions'
 import { Button } from 'react-bootstrap'
 
 const LoginStatus = (props) => {
-  console.log('props', props)
-  return (
-    <Button bsStyle="primary" className="navbar-btn" onClick={props.auth.login.bind(this)}>Login</Button>
-  )
+  const auth = props.auth
+
+  if (auth.loggedIn) {
+    return (<span>AWESOME!</span>)
+  } else {
+    return <Button bsStyle="primary" className="navbar-btn" onClick={auth.login.bind(this)}>Login</Button>
+  }
 }
 
 export default LoginStatus
