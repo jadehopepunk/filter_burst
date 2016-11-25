e61fb0d50b2e4062877db57c825de4832a6d8029
craigambrose <craig@craigambrose.com>
2016-11-25 14:36:16 +1300

    set the node env in web pack for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,3 +1,10 @@
 var config = require('./webpack.common.config')
+var webpack = require('webpack');
+
+config.plugins.unshift(
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+  })
+)
 
 module.exports = config
