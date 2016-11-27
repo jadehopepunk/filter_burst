c7e0e3662efcfc8a57285e6492ebfffd94ea1032
craigambrose <craig@craigambrose.com>
2016-11-27 20:10:19 +1300

    starting to flesh out grapnel schema for creating posts

---
+++ web/resolvers/post.ex
@@ -0,0 +1,8 @@
+defmodule FilterBurst.Resolvers.Post do
+  # alias FilterBurst.FacebookUser
+  # alias FilterBurst.User
+
+  def create(_parent, params, _info) do
+    {:ok, nil}
+  end
+end
--- web/schema.ex
+++ web/schema.ex
@@ -2,5 +2,24 @@ defmodule FilterBurst.Schema do
   use Absinthe.Schema
   # use Absinthe.Relay.Schema
 
+  object :post do
+    field :id, :id
+    field :user_id, :string
+    field :text, :string
+  end
 
+  mutation do
+    field :create_post, type: :post do
+      arg :user_id, non_null(:string)
+      arg :text, non_null(:string)
+
+      resolve &FilterBurst.Resolvers.Post.create/3
+    end
+  end
+
+  query do
+    # field :get_node, :node do
+    #   arg :id, non_null(:string)
+    # end
+  end
 end
